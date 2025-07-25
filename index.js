9f09300915704006c6ed215f3b56562e00d31e7f
ivanuc21 <ivanuc172021@gmail.com>
2025-07-25 12:06:04 -0400

    Crear funcionalidad para crear y guardar nueva tarea

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ document.addEventListener('DOMContentLoaded', function(){
 		addTask(element.text, element.completed)
 	});
 })
+
+/**
+ * Al boton de agregar tarea se le agrega un esuchador de eventos para acptura cuando
+ * se le da click
+ */
+taskAddButton.addEventListener('click', function(){
+	// se obtiene el valor ingresado por el usuario y se quitan espacios en caso de haber
+	const taskValue = taskInput.value.trim();
+	// se evalua para asegurar que no este vacio 
+	if(taskValue){
+		// se obtienen las tareas almacenadas en local storage
+		const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+		// se agrega al array obtenido, el objeto con los atributos y sus respectivos valores
+		tasks.push({ text: taskValue, completed: false})
+
+		// se actualiza el localstorage 
+		localStorage.setItem('tasks', JSON.stringify(tasks))
+		// se agrega la tarea nueva al documento y luego se limpia el campo de entrada
+		addTask(taskValue, false)
+		taskInput.value = ''
+	}
+})
+
