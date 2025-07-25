047eb3acb7b0a17eb84fd1f534cc8123d2158968
ivanuc21 <ivanuc172021@gmail.com>
2025-07-25 12:55:48 -0400

    Crear funcion para agregar tareas en la lista del documento

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const taskAddButton = document.getElementById('task-add-button')
 const taskList = document.getElementById('task-list')
 
 
-const messageContainer = document.getElementById('message-main-container')
 /**
  * Al cargarse el documento se ejecutara la funcion que cargara los elementos(tareas)
  * al documento, utiliando la funcion 'addTasks'
  */
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 	/*
 	Obtener tareas desde el local storage, en caso de que aun no hayan elementos
 	, se devuelve un array vacio */
 	const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
-    tasks.forEach(element => {
+	tasks.forEach(element => {
 		addTask(element.text, element.completed)
 	});
 })
@@ -22,15 +21,15 @@ document.addEventListener('DOMContentLoaded', function(){
  * Al boton de agregar tarea se le agrega un esuchador de eventos para acptura cuando
  * se le da click
  */
-taskAddButton.addEventListener('click', function(){
+taskAddButton.addEventListener('click', function () {
 	// se obtiene el valor ingresado por el usuario y se quitan espacios en caso de haber
 	const taskValue = taskInput.value.trim();
 	// se evalua para asegurar que no este vacio 
-	if(taskValue){
+	if (taskValue !== '') {
 		// se obtienen las tareas almacenadas en local storage
 		const tasks = JSON.parse(localStorage.getItem('tasks') || '[]')
 		// se agrega al array obtenido, el objeto con los atributos y sus respectivos valores
-		tasks.push({ text: taskValue, completed: false})
+		tasks.push({ text: taskValue, completed: false })
 
 		// se actualiza el localstorage 
 		localStorage.setItem('tasks', JSON.stringify(tasks))
@@ -39,4 +38,51 @@ taskAddButton.addEventListener('click', function(){
 		taskInput.value = ''
 	}
 })
+
+function addTask(text, completed = false) {
+	const li = document.createElement('li');
+	li.classList.add('mb-2')
+	if (completed) li.classList.add('done')
+
+	const taskSpan = document.createElement('span')
+	taskSpan.textContent = text
+	taskSpan.addEventListener('click', function () {
+		li.classList.toggle('done')
+		updateStorage()
+	})
+
+	const modalMessageAccept = document.getElementById('message-main-container');
+	const succesModal = document.getElementById('success-message-container');
+	const acceptButon = document.getElementById('accept-button')
+	const cancelButton = document.getElementById('cancel-button');
+	const successButton = document.getElementById('success-button');
+
+	const deleteButton = document.createElement('button');
+	deleteButton.classList.add('btn', 'btn-danger', 'm-2')
+	deleteButton.textContent = 'Eliminar'
+
+	deleteButton.addEventListener('click', function () {
+
+		modalMessageAccept.style.display = 'flex'
+		acceptButon.addEventListener('click', function () {
+			modalMessageAccept.style.display = 'none'
+			li.remove()
+
+			succesModal.style.display = 'flex'
+			updateStorage()
+			successButton.addEventListener('click', function () {
+				succesModal.style.display = 'none'
+			})
+		})
+		cancelButton.addEventListener('click', function(){
+			modalMessageAccept.style.display = 'none'
+		})
+	})
+
+	li.appendChild(taskSpan)
+	li.appendChild(deleteButton)
+	taskList.appendChild(li)
+}
+
+
 
